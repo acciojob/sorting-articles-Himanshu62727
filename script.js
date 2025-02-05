a6c2f192721d23974bbaa04ee7138e78a0cab15d
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2025-02-05 20:44:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-//your JS code here. If required.
-
-const ul = document.querySelector('ul');
-
-
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-
-function sortArticles(articles) {
-    return articles.sort((a, b) => {
-        const removeArticles = (str) => str.replace(/^(a|an|the)\s/i, "");
-
-        const aWithoutArticles = removeArticles(a);
-        const bWithoutArticles = removeArticles(b);
-
-        return aWithoutArticles.localeCompare(bWithoutArticles);
-    });
+function strip(bandName) {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
 }
 
-sortArticles(bands);
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-bands.forEach(band => {
+const bandList = document.getElementById('band');
+
+sortedBands.forEach(band => {
     const li = document.createElement('li');
     li.textContent = band;
-    ul.appendChild(li);
+    bandList.appendChild(li);
 });
