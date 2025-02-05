c354063086a19bddb1527d5a78d78ba201ac0030
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2025-02-05 20:49:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+const ul = document.querySelector('ul');
+
+
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function strip(bandName) {
-    return bandName.replace(/^(a |an |the )/i, '').trim();
+
+function sortArticles(articles) {
+    return articles.sort((a, b) => {
+        const removeArticles = (str) => str.replace(/^(a|an|the)\s/i, "");
+
+        const aWithoutArticles = removeArticles(a);
+        const bWithoutArticles = removeArticles(b);
+
+        return aWithoutArticles.localeCompare(bWithoutArticles);
+    });
 }
 
-const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+sortArticles(bands);
 
-const bandList = document.getElementById('bands'); // Updated id from "band" to "bands"
-
-sortedBands.forEach(band => {
+bands.forEach(band => {
     const li = document.createElement('li');
     li.textContent = band;
-    bandList.appendChild(li);
+    ul.appendChild(li);
 });
